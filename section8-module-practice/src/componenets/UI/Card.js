0476dc85e1b7c46a300034ab82132ce42d01016f
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-14 01:24:44 +0900

    lifting state up

---
+++ section8-module-practice/src/componenets/UI/Card.js
@@ -0,0 +1,9 @@
+import classes from './Card.modul.css';
+
+const Card = (props) => {
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
+};
+
+export default Card;
