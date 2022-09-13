952f7eaf488f28186cfb553ee0dbfa55f96ab64b
Lee Jaeha <108874515+jaehafe@users.noreply.github.com>
2022-09-13 23:52:02 +0900

    section 3  - React Components

---
+++ section3-react-component/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,24 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+import Card from '../UI/Card';
+
+function ExpenseItem({ title, amount, date }) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+      <button
+        onClick={() => {
+          console.log('clicked');
+        }}
+      >
+        Change Title
+      </button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
